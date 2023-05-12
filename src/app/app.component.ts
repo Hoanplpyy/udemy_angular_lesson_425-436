66c1ec5dbfc0f3fdfc651430619e8a6d7e0fdafd
hsien <hoanplpyy@hotmail.com>
2023-05-12 09:54:26 +0800

    428: 加入trigger,state的動畫設定，並設定元件樣式。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+    ])
+  ]
 })
 export class AppComponent {
+
+  state = 'normal';
 
   list = ['Milk', 'Sugar', 'Bread'];
 
